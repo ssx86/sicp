a0a4168dd04a63e5af9022234c80b1a988e49e12
ssx86 <zss2366@gmail.com>
2015-06-30 16:33:01 +0000

    1.8 correct

--- chapter1/1.8.rkt
+++ chapter1/1.8.rkt
@@ -1,25 +1,27 @@
 #lang racket
 
 (define (subtriplicate x)
-  (subtriplicate-iter 1 x))
 
-(define (subtriplicate-iter guess x)
-  (printf "~a ~a\n" (exact->inexact guess) x)
-  (if (good-enough? guess x)
-    (exact->inexact guess)
-    (subtriplicate-iter (improve guess x) x)))
+  (define (subtriplicate-iter guess)
+    (if (good-enough? guess)
+      guess
+      (subtriplicate-iter (improve guess)) 
+      ))
 
-(define (good-enough? x y)
-  (print "good")
-  (< (abs (- (* x x x) y)) 0.001))
+  (define (good-enough? y)
+    (< (abs (- (* y y y) x)) 1))
 
-(define (improve guess x)
-  (print "improve")
-  (/ 
-    (+ (/ x (* guess guess)) 
-      (* 2 guess)) 
-    3))
+  (define (improve guess)
+    (/ 
+      (+ (/ x (* guess guess)) 
+         (* 2 guess)) 
+      3))
+
+  (subtriplicate-iter 1.0))
+
 
 (subtriplicate 8)
 (subtriplicate 1000)
 
+;; 这里遇到一个大坑，iter调用的时候初始如果传递1，会迭代速度异常慢
+;; 看别人答案发现是1.0，尝试了一下就可以求解了，暂时还不知道为什么。
