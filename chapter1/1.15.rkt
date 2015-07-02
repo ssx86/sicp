a22de0344c45583b3ec7e4d723980c0ec5709791
ssx86 <zss2366@gmail.com>
2015-07-03 00:00:31 +0800

    1.15

--- chapter1/1.15.rkt
+++ chapter1/1.15.rkt
@@ -1,1 +1,20 @@
 #lang racket
+(define (cube x) (* x x x))
+
+(define (p x)
+  (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
+
+(sine 12.15)
+(p (sine 4.05))
+(p (p (sine 1.35)))
+(p (p (p (sine 0.45))))
+(p (p (p (p (sine 0.15)))))
+(p (p (p (p (p (sine 0.05))))))
+;; a) 5次调用p
+;; b) 步数增长的阶是以3为底的对数
+;;    空间同样是以3为底的对数
