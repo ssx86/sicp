95304f89f36758fd3be2383299239fac868d58f9
ssx86 <zss2366@gmail.com>
2015-07-02 22:43:51 +0800

    add all code filesgit status!

--- gen.rb
+++ gen.rb
@@ -1,12 +1,32 @@
-chapter = ARGV[0]
-id = ARGV[1]
-
-dir_name = "chapter#{chapter}"
-unless File.exist? dir_name
-  Dir.mkdir dir_name
+def create_file(file_name)
+  outfile=File.new(file_name,"w")
+  outfile.puts "#lang racket\n"
+  outfile.close
 end
 
-file_name = "#{dir_name}/#{chapter}.#{id}.rkt"
-outfile=File.new(file_name,"w")
-outfile.puts "#lang racket\n"
-outfile.close
+def make_dir(chapter, id)
+  dir_name = "chapter#{chapter}"
+  unless File.exist? dir_name
+    Dir.mkdir dir_name
+  end
+
+  file_name = "#{dir_name}/#{chapter}.#{id}.rkt"
+
+  create_file file_name unless File.exist? file_name
+end
+
+@chapter_info = [
+  {chapter: 1, max_id: 40},
+  {chapter: 2, max_id: 97},
+  {chapter: 3, max_id: 82},
+  {chapter: 4, max_id: 79},
+  {chapter: 5, max_id: 52}
+]
+
+@chapter_info.each do |info|
+  chapter = info[:chapter]
+  max_id = info[:max_id]
+  (1..max_id).each do |id|
+    make_dir chapter, id
+  end
+end
