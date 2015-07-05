b03a8c176477adc4c9248dfb5b8b55e027897234
ssx86 <zss2366@gmail.com>
2015-07-06 00:25:20 +0800

    1.16.rkt

--- chapter1/1.16.rkt
+++ chapter1/1.16.rkt
@@ -14,3 +14,18 @@
 (fast-expt 3 3)
 
 ;; 实在读不懂题！！抓狂啊
+;; 2015.7.5今天貌似突然看明白了
+;; 试试看
+(define (fast-expt-a b n)
+  (define (iter b n a)
+    (cond ((= n 0) a)
+          (else (cond ((even? n)
+                       (iter (square b) (/ n 2) a))
+                      (else
+                       (iter b (- n 1) (* a b))
+                       )))))
+  (iter b n 1))
+
+(fast-expt-a 3 4)
+;;貌似是这个意思吧。
+;;注意一个问题，(/ n 2)又写成了(/ 2 n)
