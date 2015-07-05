056280ff26364fe69b2e21998616e8ad09f094b1
ssx86 <zss2366@gmail.com>
2015-07-06 00:38:18 +0800

    1.17, 1.18 done

--- chapter1/1.17.rkt
+++ chapter1/1.17.rkt
@@ -1,1 +1,15 @@
 #lang racket
+
+;; 加法代替乘法
+(define (halve x)
+  (/ x 2))
+
+(define (double x)
+  (* x 2))
+
+(define (fast-multi x n)
+  (cond ((= n 0) 0)
+        ((even? n) (fast-multi (double x) (halve n)))
+        (else (+ x (fast-multi x (- n 1))))))
+
+(fast-multi 4 9)
--- chapter1/1.18.rkt
+++ chapter1/1.18.rkt
@@ -1,1 +1,13 @@
 #lang racket
+(define (double x)
+  (* x 2))
+
+(define (halve x)
+  (/ x 2))
+
+(define (multi x y)
+  (cond ((= y 1) x)
+        ((even? y) (multi (double x) (halve y)))
+        (else (+ x (multi x (- y 1))))))
+
+(multi 10 32)
