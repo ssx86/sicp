ae3069117f52dc21ad69cbfdf6d3e84cede3db51
ssx86 <zss2366@gmail.com>
2015-07-06 16:33:19 +0000

    1.21 100%

--- chapter1/1.21.rkt
+++ chapter1/1.21.rkt
@@ -1,1 +1,13 @@
 #lang racket
+(define (smallest-divisor x)
+  (find-divisor x 2))
+
+(define (find-divisor x test-divisor)
+  (cond ((> (* test-divisor test-divisor) x) x)
+        ((= 0 (remainder x test-divisor)) test-divisor)
+        (else (find-divisor x (+ test-divisor 1)))))
+
+(smallest-divisor 199)
+(smallest-divisor 1999)
+(smallest-divisor 19999)
+
