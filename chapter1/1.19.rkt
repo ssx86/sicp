379e6ee52b238707433fabeac2001e5bad49e98a
SS-X <zss2366@gmail.com>
2015-07-06 13:13:42 +0800

    1.19 100%

--- chapter1/1.19.rkt
+++ chapter1/1.19.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;; 先抄题
+;; 先正确的抄题
 (define (fib n)
   (fib-iter 1 0 0 1 n))
 
@@ -9,11 +9,44 @@
         ((even? count)
          (fib-iter a
                    b
-                   <??>
-                   <??>
+                   (+ (* p p) (* q q))
+                   (+ (* 2 p q) (* q q))
                    (/ count 2)))
         (else (fib-iter (+ (* b q) (* a q) (* a p))
-                        (+ (* b q) (* a q))
+                        (+ (* b p) (* a q))
                         p
                         q
                         (- count 1)))))
+#|
+a' -> bq + aq + ap
+b' -> bp + aq
+
+a'' -> (bp + aq)q + (bq + aq + ap)q + (bq + aq + ap)p
+    -> qpb + qqa + qqb + qqa + qpa + qpb + qpa + ppa
+    -> (qq + qq + qp + qp + pp)a + (qp + qq + qp)b
+    -> (2qq + 2qp + pp)a + (2qp + qq)b
+!!!!-> (2qp + qq)a + (2qp + qq)b + (qq + pq)a
+
+b'' -> (bp + aq)p + (bq + aq + ap)q
+    -> ppb + qpa + qqb + qqa + qpa
+    -> (qp + qq + qp)a + (pp + qq)b
+!!!!-> (2qp + qq)a + (pp + qq)b
+
+
+so:
+q' -> (2qp + qq)
+p' -> (pp + qq)
+
+|#
+
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 4)
+(fib 5)
+(fib 6)
+(fib 7)
+(fib 8)
+(fib 9)
+(fib 10)
+
