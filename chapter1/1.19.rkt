be1ced212bbc1b85eefcbb197d52519d7a71dce2
ssx86 <zss2366@gmail.com>
2015-07-06 08:26:58 +0800

    1.19 1%

--- chapter1/1.19.rkt
+++ chapter1/1.19.rkt
@@ -1,1 +1,19 @@
 #lang racket
+
+;; 先抄题
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   <??>
+                   <??>
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b q) (* a q))
+                        p
+                        q
+                        (- count 1)))))
