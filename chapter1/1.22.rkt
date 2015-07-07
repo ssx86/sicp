85e225171eac7405252896e7d648665680ec45f0
ssx86 <zss2366@gmail.com>
2015-07-07 08:16:50 +0800

    1.22 1%

--- chapter1/1.22.rkt
+++ chapter1/1.22.rkt
@@ -1,1 +1,16 @@
 #lang racket
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (current-milliseconds)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (current-milliseconds) start-time))
+      #t))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+(timed-prime-test 100)
