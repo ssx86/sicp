06b88cc9d42a84dba7d936f5d304f383ae2b9c7f
ssx86 <zss2366@gmail.com>
2015-07-08 23:26:41 +0800

    1.22 some changes

--- chapter1/1.22.rkt
+++ chapter1/1.22.rkt
@@ -12,13 +12,6 @@
 (define (prime? x)
   (= x (smallest-divisor x)))
 
-;; 费马定律需要 得到一个数的幂对另一个数的模
-(define (expmod base exp n)
-  (cond ((= exp 0) 1)
-        ((even? exp) 
-         (remainder (square (expmod base (/ exp 2) n)) n))    ;;这里base没有直接平方，而是在取了模之后平方。第一次写错了
-        (else 
-         (remainder (* base (expmod base (- exp 1) n)) n))))
 
 ;; TODO: 第二遍时重新默写一次expmod
 
@@ -29,15 +22,16 @@
   (= (remainder x 2) 0))
 
 ;; TODO: 有一个疑问。if必须写else，那么我如果只需要在成立的时候做某个事情，如何才能不写空的else？
+;; http://stackoverflow.com/questions/5751052/how-does-if-statement-work-in-scheme
+;; http://stackoverflow.com/questions/10863192/why-is-one-armed-if-missing-from-racket
 (define (timed-prime-test n)
   (newline)
   (display n)
   (start-prime-test n (current-milliseconds)))
 
 (define (start-prime-test n start-time)
-  (if (prime? n)
-      (report-prime (- (current-milliseconds) start-time))
-      #f))
+  (when (prime? n)
+      (report-prime (- (current-milliseconds) start-time))))
 
 (define (report-prime elapsed-time)
   (display " *** ")
@@ -54,8 +48,15 @@
 (find-3-prime 1000)
 (find-3-prime 10000)
 (find-3-prime 100000)
-(find-3-prime 100000000)
+(find-3-prime 1000000)
 
 ;;貌似精度不足。留下问题下面再处理了
 
 
+;; 费马定律需要 得到一个数的幂对另一个数的模
+(define (expmod base exp n)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) n)) n))    ;;这里base没有直接平方，而是在取了模之后平方。第一次写错了
+        (else
+         (remainder (* base (expmod base (- exp 1) n)) n))))
