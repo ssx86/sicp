531606af0c5e414497bb3e3c35659aede76a4b31
ssx86 <zss2366@gmail.com>
2015-07-13 00:45:11 +0800

    1.32

--- chapter1/1.32.rkt
+++ chapter1/1.32.rkt
@@ -1,1 +1,23 @@
 #lang racket
+
+(define (accumulate combiner null-value term a next b)
+  (if (> a b)
+      null-value
+      (combiner (term a) (accumulate combiner null-value term (next a) next b))))
+
+(define (accumulate2 combiner null-value term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (combiner result (term a)))))
+  (iter a null-value))
+
+
+(define (sum a b)
+  (accumulate (lambda(x y) (+ x y)) 0 (lambda(x) x) a (lambda(x) (+ x 1)) b))
+
+(define (sum2 a b)
+  (accumulate2 (lambda(x y) (+ x y)) 0 (lambda(x) x) a (lambda(x) (+ x 1)) b))
+
+(sum 0 10)
+(sum2 0 10)
