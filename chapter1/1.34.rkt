1bc902e41c1f6efcb58b4762ae5ba845fd002814
SS-X <zss2366@gmail.com>
2015-07-13 19:11:24 +0800

    1.34

--- chapter1/1.34.rkt
+++ chapter1/1.34.rkt
@@ -1,1 +1,12 @@
 #lang racket
+(define (f g)
+  (g 2))
+
+(define (square x)
+  (* x x))
+
+(f square)
+(f (lambda (z) (* z (+ z 1))))
+(f f)
+
+;;(f f)在调用(g 2)时，实际上扩展成了(g g)需要一个procedure，但是提供了2所以无法执行，出错
