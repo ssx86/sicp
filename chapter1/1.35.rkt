4a9fadb256d05a78835a878423aa5a43d3c09ded
ssx86 <zss2366@gmail.com>
2015-07-15 23:29:04 +0800

    1.35

--- chapter1/1.35.rkt
+++ chapter1/1.35.rkt
@@ -1,1 +1,17 @@
 #lang racket
+;;;证明硬伤。瞎写咯
+;; 黄金分割： x^2 = x + 1, 两边/x得到 x = 1 + 1/x 证毕。呵呵呵
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(exact->inexact (fixed-point (lambda(x) (+ 1 (/ 1 x))) 1))
