12b75ca2e81dd76b7b86a95c6f9cf586c8c06d4b
ssx86 <zss2366@sina.com>
2015-07-22 01:01:18 +0800

    2.6 0%

--- chapter2/2.6.rkt
+++ chapter2/2.6.rkt
@@ -1,1 +1,19 @@
 #lang racket
+;;终于到这个玩意儿了。。
+;; church 计数
+
+;;接受任何函数，返回一个y = x的函数
+(define zero (lambda (f) (lambda (x) x)))
+
+(define (add-1 n) (lambda (f) (lambda (x) (f ((n f) x)))))
+
+;; so: one = (add-1 zero)
+;;(define one (add-1 zero))
+;;(define one (lambda (f) (lambda (x) (f ((zero f) x)))))
+;;(define one (lambda (f) (lambda (x) (f (lambda (x) x)))))
+
+
+;; (define (zero f x) x)
+;; (define (one f x) f(lambda (x) x))
+
+;;实在不会。。。。。
