cc8144795a7c3dd898e4ebcd0abc1abc594bf101
SS-X <zss2366@gmail.com>
2015-07-21 20:32:47 +0800

    1.46 95%

--- chapter1/1.46.rkt
+++ chapter1/1.46.rkt
@@ -20,18 +20,31 @@
 
 (define (improve guess x)
   (average guess (/ x guess)))
-
-;; 1.3.3 fixed-point
-(define (fixed-point f guess)
-  (define (close-enough? x y)
-    (< (abs (- x y)) 0.00001))
-  (let ((next (f guess)))
-    (if (close-enough? guess next)
-        next
-        (fixed-point f next))))
+;
+;;; 1.3.3 fixed-point
+;(define (fixed-point f guess)
+;  (define (close-enough? x y)
+;    (< (abs (- x y)) 0.00001))
+;  (let ((next (f guess)))
+;    (if (close-enough? guess next)
+;        next
+;        (fixed-point f next))))
 
 ;;(fixed-point (lambda (y) (+ 1 (/ 1 y))) 0.1)
 
 (define (iterative-improve judge-f improve-f)
-  )
+  (define (iter guess)
+    (let ((next (improve-f guess)))
+      (if (judge-f guess next)
+          next
+          (iter next))))
+  iter)
+
+(define (fixed-point f guess)
+  (define (close-enough? x y)
+    (< (abs (- x y)) 0.00001))
+  ((iterative-improve close-enough? f) guess))
+
+
+(fixed-point (lambda (y) (+ 1 (/ 1 y))) 0.1)       
 
