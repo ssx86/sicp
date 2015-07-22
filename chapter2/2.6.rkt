0eba4232515e33507c3242c4e08ca182c0d34ed9
SS-X <zss2366@gmail.com>
2015-07-22 15:08:34 +0800

    2.6 100% oh yeah!

--- chapter2/2.6.rkt
+++ chapter2/2.6.rkt
@@ -8,12 +8,29 @@
 (define (add-1 n) (lambda (f) (lambda (x) (f ((n f) x)))))
 
 ;; so: one = (add-1 zero)
-;;(define one (add-1 zero))
-;;(define one (lambda (f) (lambda (x) (f ((zero f) x)))))
-;;(define one (lambda (f) (lambda (x) (f (lambda (x) x)))))
+;(define one (add-1 zero))
+;(define one (lambda (f) (lambda (x) (f ((zero f) x)))))
+;(define one (lambda (f) (lambda (x) (f ((lambda (x) x) x)))))
+(define one (lambda (f) (lambda (x) (f x))))
 
+;(define two (add-1 one))
+;(define two (lambda (f) (lambda (x) (f ((one f) x)))))
+;(define two (lambda (f) (lambda (x) (f ((lambda (x) (f x)) x)))))
+(define two (lambda (f) (lambda (x) (f (f x)))))
 
-;; (define (zero f x) x)
-;; (define (one f x) f(lambda (x) x))
+(define three (lambda (f) (lambda (x) (f (f (f x))))))
 
-;;实在不会。。。。。
+(define (inc x)
+  (+ x 1))
+
+((zero inc) 0)
+((one inc) 0)
+((two inc) 0)
+((three inc) 0)
+
+(define (add m n) (lambda (f) (lambda (x) ((m f) ((n f) x)))))
+
+(define five (add two three))
+(define eight (add five three))
+((eight inc) 10)
+
