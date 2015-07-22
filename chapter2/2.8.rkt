cb2a9dc0220d211cc1afd5aebdede757db041200
SS-X <zss2366@gmail.com>
2015-07-22 18:03:14 +0800

    2.8

--- chapter2/2.8.rkt
+++ chapter2/2.8.rkt
@@ -1,1 +1,15 @@
 #lang racket
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+
+(define (make-interval a b) (cons a b))
+
+
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(sub-interval (make-interval -9 3) (make-interval 2 4))
+(sub-interval (make-interval 0 39) (make-interval 30 31))
