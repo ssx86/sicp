b72407a8ac4a2bb20224e71204f13212d588761a
SS-X <zss2366@gmail.com>
2015-07-28 18:25:04 +0800

    2.19

--- chapter2/2.19.rkt
+++ chapter2/2.19.rkt
@@ -1,1 +1,31 @@
 #lang racket
+
+(define us-coins (list 50 25 10 5 1))
+(define uk-coins (list 100 50 20 10 5 2 1 0.5))
+
+(define (count-charge amount coin-values)
+  (cc amount coin-values))
+
+(define (cc amount coin-values)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (no-more? coin-values)) 0)
+        (else
+         (+ (cc amount
+                (except-first-denomination coin-values))
+            (cc (- amount
+                   (first-denomination coin-values))
+                coin-values)))))
+
+(define (first-denomination coin-values)
+  (car coin-values))
+
+(define (except-first-denomination coin-values)
+  (cdr coin-values))
+
+(define (no-more? coin-values)
+  (null? coin-values))
+
+(count-charge 100 us-coins)
+(count-charge 100 uk-coins)
+
+;;并不会影响cc的答案，列表最终都会被遍历完
