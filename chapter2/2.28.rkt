2cbcd5583d2bcf77edeb91c3db20e5258a85a963
ssx86 <zss2366@sina.com>
2015-07-29 23:40:08 +0800

    2.28

--- chapter2/2.28.rkt
+++ chapter2/2.28.rkt
@@ -1,1 +1,10 @@
 #lang racket
+
+(define (fringe l)
+  (cond ((null? l) '())
+        ((pair? l) (append (fringe (car l)) (fringe (cdr l))))
+        (else (list l))))
+
+(define x (list (list 1 2) (list 3 4)))
+(fringe x)
+(fringe (list x (list x x (list x))))
