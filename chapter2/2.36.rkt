e43ae59acf15f4e851480f920e70ed07a009e8af
SS-X <zss2366@gmail.com>
2015-08-01 17:48:59 +0800

    2.36

--- chapter2/2.36.rkt
+++ chapter2/2.36.rkt
@@ -1,1 +1,17 @@
 #lang racket
+
+(define (accumulate op init seqs)
+  (if (null? seqs)
+      init
+      (op (car seqs)
+          (accumulate op init (cdr seqs)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      '()
+      (cons (accumulate op init (map (λ (lst) (car lst)) seqs))
+            (accumulate-n op init (map (λ (lst) (cdr lst)) seqs)))))
+
+(accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)))
+
+;;这题有意思
