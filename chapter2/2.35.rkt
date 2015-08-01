a9162d451ca90a497aa1b0fd92149733b7d468b8
SS-X <zss2366@gmail.com>
2015-08-01 17:41:00 +0800

    2.35

--- chapter2/2.35.rkt
+++ chapter2/2.35.rkt
@@ -1,1 +1,16 @@
 #lang racket
+
+(define (accumulate op init seq)
+  (if (null? seq)
+      init
+      (op (car seq)
+          (accumulate op init (cdr seq)))))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (λ (x)
+                         (if (not (pair? x))
+                             1
+                             (count-leaves x)))
+                       t)))
+
+(count-leaves (list 1 2 (list (list 3 4) 5 (list 3 6))))
