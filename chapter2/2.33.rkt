4ea8132bed41a9ee3924a8ad0356cdaaa04fe1bb
SS-X <zss2366@gmail.com>
2015-08-01 17:04:12 +0800

    2.32 2.33

--- chapter2/2.33.rkt
+++ chapter2/2.33.rkt
@@ -1,1 +1,28 @@
 #lang racket
+
+(define (accumulate f init seq)
+  (if (null? seq)
+      init
+      (f (car seq)
+         (accumulate f init (cdr seq)))))
+
+;; (map)
+(define (map p sequence)
+  (accumulate (λ (x y) (cons (p x) y)) '() sequence))
+
+(map (λ (x) (* x 2)) (list 2 3 4 5))
+(map (λ (x) (* x x)) (list 2 3 4 5))
+
+;; (append)
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(append (list 3 4 5) (list 6 7 8))
+
+;; (length)
+(define (length sequence)
+  (accumulate (λ (x y) (+ y 1)) 0 sequence))
+
+(length (list 3 4 4 3 3 4 4 3)) ; 8
+
+;;这个题很有意思。mark
--- chapter2/2.34.rkt
+++ chapter2/2.34.rkt
@@ -1,1 +1,15 @@
 #lang racket
+
+(define (accumulate op init sequence)
+  (if (null? sequence)
+      init
+      (op (car sequence)
+          (accumulate op init (cdr sequence)))))
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (λ (this-coeff higher-terms)
+                (+ this-coeff (* higher-terms x)))
+              0
+              coefficient-sequence))
+
+(horner-eval 2 (list 1 3 0 5 0 1))
