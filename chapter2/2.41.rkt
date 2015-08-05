45240901ffdd4063eabb2e83ea656d878acf86ca
ssx86 <zss2366@gmail.com>
2015-08-06 01:40:07 +0800

    2.41

--- chapter2/2.41.rkt
+++ chapter2/2.41.rkt
@@ -1,1 +1,36 @@
 #lang racket
+
+;;生成序列
+(define (enumerate-interval begin end)
+  (if (> begin end)
+      null
+      (cons begin (enumerate-interval (+ begin 1) end))))
+(define (flatmap proc seq)
+  (accumulate append '() (map proc seq)))
+
+(define (accumulate op init seq)
+  (if (null? seq)
+      init
+      (op (car seq)
+          (accumulate op init (cdr seq)))))
+
+;;resolver
+(define (gen-three-number max)
+  (flatmap
+   (λ (i)
+     (flatmap 
+      (λ (j)
+        (map 
+         (λ (k)
+           (list k j i))
+         (enumerate-interval 1 (- j 1))))
+      (enumerate-interval 1 (- i 1))))
+   (enumerate-interval 1 (- max 1))))
+
+(gen-three-number 7)
+(define (gen-sum n)
+  (filter
+   (λ (lst)
+     (= (accumulate + 0 lst) n))
+   (gen-three-number n)))
+(gen-sum 9)
