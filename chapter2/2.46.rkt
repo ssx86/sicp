493b447e4a8c1c5fb743c97ee22d9f71164cd184
ssx86 <zss2366@gmail.com>
2015-08-09 16:31:04 +0800

    2.46
    2.46

--- chapter2/2.46.rkt
+++ chapter2/2.46.rkt
@@ -1,1 +1,24 @@
 #lang racket
+
+(define (make-vect x y)
+  (list x y))
+(define (xcor-vect v)
+  (car v))
+(define (ycor-vect v)
+  (cadr v))
+
+(define (add-vec x y)
+  (make-vect (+ (xcor-vect x)
+                (xcor-vect y))
+             (+ (ycor-vect x)
+                (ycor-vect y))))
+
+(define (sub-vec x y)
+  (make-vect (- (xcor-vect x)
+                (xcor-vect y))
+             (- (ycor-vect x)
+                (ycor-vect y))))
+
+(define (scale-vect v n)
+  (make-vect (* n (xcor-vect v))
+             (* n (ycor-vect v))))
