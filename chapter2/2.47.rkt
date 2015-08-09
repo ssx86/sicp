f5e894b06ef726b3db4f9ac2089753367472dc75
ssx86 <zss2366@gmail.com>
2015-08-09 16:36:31 +0800

    2.47
    2.47

--- chapter2/2.47.rkt
+++ chapter2/2.47.rkt
@@ -1,1 +1,27 @@
 #lang racket
+;
+;(define (make-frame origin edge1 edge2)
+;  (list origin edge1 edge2))
+;(define (origin-frame frame)
+;  (car frame))
+;(define (edge1-frame frame)
+;  (cadr frame))
+;(define (edge2-frame frame)
+;  (caddr frame))
+;
+
+
+(define (make-frame origin edge1 edge2)
+  (cons origin (cons edge1 edge2)))
+(define (origin-frame frame)
+  (car frame))
+(define (edge1-frame frame)
+  (cadr frame))
+(define (edge2-frame frame)
+  (cddr frame))
+
+;test
+
+(origin-frame (make-frame 3 4 5))
+(edge1-frame (make-frame 3 4 5))
+(edge2-frame (make-frame 3 4 5))
