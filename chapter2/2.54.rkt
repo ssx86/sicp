a86583ffb771d5246fd071b2ee58fdace6ad6b0c
ssx86 <zss2366@gmail.com>
2015-08-11 00:01:44 +0800

    2.54
    2.54

--- chapter2/2.54.rkt
+++ chapter2/2.54.rkt
@@ -1,1 +1,25 @@
 #lang racket
+
+;;是否可以使用atom?
+(define (atom? a)
+  (and (not (null? a))
+       (not (pair? a))))
+
+;(atom? '1)
+;(atom? '(3 4))
+;(atom? 'a)
+
+(define (equal? l1 l2)
+  (if (or (and (null? l1) (null? l2))
+          (and (atom? l1) (atom? l2) (eq? l1 l2))
+          (and (pair? l1)
+               (pair? l2)
+               (equal? (car l1) (car l2))
+               (equal? (cdr l1) (cdr l2))))
+      #t
+      #f))
+
+(equal? '() '())
+(equal? '(a b c) '(a b c))
+(equal? 'b 'c)
+(equal? '(a b c) 3)
