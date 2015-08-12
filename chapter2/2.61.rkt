4ddeb6e289abdbe305f69e6649b43147927f11d2
ssx86 <zss2366@gmail.com>
2015-08-13 00:59:54 +0800

    2.61
    2.61

--- chapter2/2.61.rkt
+++ chapter2/2.61.rkt
@@ -1,1 +1,13 @@
 #lang racket
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? x (car set)) #t)
+        (else (element-of-set? x (cdr set)))))
+                            
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< x (car set)) (cons x set))
+        (else (cons (car set) (adjoin-set x (cdr set))))))
+
+(adjoin-set 3 '(1 2 4 6))
