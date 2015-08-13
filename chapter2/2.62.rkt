c8e3aed1901564d14f593c34e118d02d0bb3bce8
ssx86 <zss2366@gmail.com>
2015-08-14 00:53:13 +0800

    2.62
    2.62

--- chapter2/2.62.rkt
+++ chapter2/2.62.rkt
@@ -1,1 +1,19 @@
 #lang racket
+;;;实现一个union-set
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((< (car set1) (car set2))
+         (cons (car set1) (union-set (cdr set1) set2)))
+        ((= (car set1) (car set2))
+         (cons (car set1) (union-set (cdr set1) (cdr set2))))
+        ((> (car set1) (car set2))
+         (cons (car set2) (union-set set1 (cdr set2))))))
+
+(union-set '(1 2 5 6 7 9) '(2 3 4 5))
+
+
+
+
+  
