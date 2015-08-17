eea2313a2f34248fc2cd065dcc0bac8e6b82265e
SS-X <zss2366@gmail.com>
2015-08-17 17:29:05 +0800

    2.67 0%

--- chapter2/2.67.rkt
+++ chapter2/2.67.rkt
@@ -23,3 +23,37 @@
   (if (leaf? tree)
       (weight-leaf tree)
       (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch branch))
+        ((= bit 1) (right-branch branch))
+        (else (error "bad bit"))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)
+                               (cadr pair))
+                    (make-leaf-set (cdr pairs))))))
+
+(define tree (make-leaf-set '((A 4) (B 2) (C 1) (D 1))))
+(decode '(1 0 0) tree)
+;;暂时没看懂，回去今天就看这个。题做到2.72！！！
