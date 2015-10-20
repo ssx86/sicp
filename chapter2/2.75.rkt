ff1e4f52d2dcb91c4827667e0055b22ec00266f4
SS-X <zss2366@gmail.com>
2015-10-20 14:18:54 +0800

    2.75

--- chapter2/2.75.rkt
+++ chapter2/2.75.rkt
@@ -1,1 +1,14 @@
 #lang racket
+
+(define (make-from-mag-ang mag ang)
+  (define (dispath op)
+    (cond ((eq? op 'real-part)
+           (* mag (cos ang)))
+          ((eq? op 'imag-part)
+           (* mag (sin ang))
+          ((eq? op 'magnitude) mag)
+          ((eq? op 'angle) ang)
+          (else
+           (error "unknown op -- MAKE-FROM-MAG-ANG" op)))))
+  dispacth)
+;;照抄，没过脑子。
