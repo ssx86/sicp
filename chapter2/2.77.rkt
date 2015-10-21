73dc43883e67a0472fda4e1f1ce1ca64933d2e86
ssx86 <zss2366@gmail.com>
2015-10-22 00:28:10 +0800

    2.77 done

--- chapter2/2.77.rkt
+++ chapter2/2.77.rkt
@@ -1,8 +1,43 @@
 #lang racket
+
+;;get and put
+
+(define global-array '())
+
+(define (make-entry k v) (list k v))
+(define (key entry) (car entry))
+(define (value entry) (cadr entry))
+
+(define (put op type item)
+  (define (put-helper k array)
+    (cond ((null? array) (list(make-entry k item)))
+          ((equal? (key (car array)) k) array)
+          (else (cons (car array) (put-helper k (cdr array))))))
+  (set! global-array (put-helper (list op type) global-array)))
+
+(define (get op type)
+  (define (get-helper k array)
+    (cond ((null? array) #f)
+          ((equal? (key (car array)) k) (value (car array)))
+          (else (get-helper k (cdr array)))))
+  (get-helper (list op type) global-array))
+
+(define (square x) (* x x))
 (define (gcd a b)
   (if (= b 0)
       a
       (gcd b (remainder a b))))
+(define (attach-tag x y) (cons x y))
+(define (type-tags z) (car z))
+(define (contents z) (cdr z))
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tags args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error "No method for these types -- APPLY-GENERIC" (list op type-tags))))))
+
 
 (define (add x y) (apply-generic 'add x y))
 (define (sub x y) (apply-generic 'sub x y))
@@ -60,3 +95,91 @@
   
   
   'done)
+
+(define (install-complex-package)
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+  
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+  
+  (define (tag z) (attach-tag 'complex z))
+  (put 'add '(complex complex)
+       (λ (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+       (λ (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+       (λ (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+       (λ (z1 z2) (tag (div-complex z1 z2))))
+  
+  (put 'make-from-real-imag 'complex
+       (λ (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+       (λ (r a) (tag (make-from-mag-ang r a))))
+  
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  
+  'done)
+;;引入rectangular
+
+(define (install-rectangular-package)
+  ;;internal procedures
+  (define (real-part z) (car z))
+  (define (imag-part z) (cdr z))
+  (define (make-from-real-imag x y) (cons x y))
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+  
+  ;;interface to the rest of the system
+  (define (tag x) (attach-tag 'rectangular x))
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  
+  (put 'make-from-real-imag 'rectangular
+       (λ (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+       (λ (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (make-complex-from-real-imag x y)
+  ((get 'make-from-real-imag 'complex) x y))
+(define (make-complex-from-mag-ang r a)
+  ((get 'make-from-mag-ang 'complex) r a))
+
+;;最终落下这几个东西。。。
+(define (real-part z) (apply-generic 'real-part z))
+(define (imag-part z) (apply-generic 'imag-part z))
+(define (magnitude z) (apply-generic 'magnitude z))
+(define (angle z) (apply-generic 'angle z))
+
+
+(install-rectangular-package)
+(install-complex-package)
+(define z (make-complex-from-real-imag 3 4))
+(magnitude z)
+
+;;出汗了。。。有空好好整理一下这几个表。现在先放过。看似已经明白了。
+;;多了一层的样子。
