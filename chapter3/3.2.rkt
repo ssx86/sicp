a13c0816b78149e26cc7e93514199673658a03a5
ssx86 <zss2366@gmail.com>
2015-11-05 01:59:43 +0800

    3.2 donw

--- chapter3/3.2.rkt
+++ chapter3/3.2.rkt
@@ -1,1 +1,18 @@
 #lang racket
+
+(define (make-monitored fun)
+  (let ([count 0])
+    (λ (param)
+      (cond [(eq? param 'how-many-calls?)
+             count]
+            [(eq? param 'reset-count)
+             (set! count 0)]
+            [else
+             (begin
+               (set! count (+ count 1))
+               (fun param))]))))
+
+(define s (make-monitored sqrt))
+(s 100)
+(s 'how-many-calls?)
+(s 9)
