6e6edfd979fcab7ed51b8176af203faefb0409e9
SS-X <zss2366@gmail.com>
2015-11-05 14:00:05 +0800

    3.3

--- chapter3/3.3.rkt
+++ chapter3/3.3.rkt
@@ -1,1 +1,25 @@
 #lang racket
+;; 修改make-account过程，使它能创建一种带密码保护的账户。
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch pwd m)
+    (if (not (eq? pwd password))
+        (λ (whatever) "Incorrect password")
+        (cond [(eq? m 'withdraw) withdraw]
+              [(eq? m 'deposit) deposit]
+              [else (error "Unknown request -- MAKE_ACCOUNT"
+                           m)])))
+  dispatch)
+
+(define acc (make-account 100 'secret-password))
+((acc 'secret-password 'withdraw) 40)
+; 60
+((acc 'some-other-password 'deposit) 50)
+; "Incorrect password"
