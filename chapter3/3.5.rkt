6780270d46914131160c73f77a4f0bb2f15c5757
ssx86 <zss2366@gmail.com>
2015-11-17 00:39:25 +0800

    3.5 done

--- chapter3/3.5.rkt
+++ chapter3/3.5.rkt
@@ -1,24 +1,9 @@
 #lang racket
-;; implement a gcd function first
 
-(define (gcd x y)
-  (if (= y 0)
-      x
-      (gcd y (remainder x y))))
+(define (square x)
+  (* x x))
 
-(define random-init 3)
-(define rand
-  (let ([x random-init])
-    (λ ()
-      (set! x (rand-update x))
-      x)))
 ;; monte-carlo
-(define (estimate-pi trials)
-  (sqrt (/ 6 (monte-carlo trials cesaro-test))))
-
-(define (cesaro-test)
-  (= (gcd (rand) (rand)) 1))
-
 (define (monte-carlo trials experiment)
   (define (iter trials-remaining trials-passed)
     (cond [(= trials-remaining 0)
@@ -26,4 +11,21 @@
           [(experiment)
            (iter (- trials-remaining 1) (+ trials-passed 1))]
           [else
-           (iter (- trials-remaining 1) trials-passed)])))
+           (iter (- trials-remaining 1) trials-passed)]))
+  (iter trials 0))
+
+(define (random-in-range low high)
+  (let ((range (- high low)))
+    (+ low (random range))))
+
+(define (estimate-integral x1 x2 y1 y2 time)
+  (monte-carlo time (λ ()
+                      (let ([m (random-in-range x1 y1)]
+                            (n (random-in-range x2 y2)))
+                        (<= (+ (square m) (square n));这里不会写了。。后面明明就是R (=1) 就行了。。不知道要抽象到什么程度
+                            (square (abs x1)))))))
+
+(define pi (* 4 (estimate-integral -100 -100 100 100 50000000)))
+(display (exact->inexact pi))
+
+;; 最后一次计算的结果是: 3.14161816
