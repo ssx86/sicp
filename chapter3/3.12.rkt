4575fd6a2ad540b30590c8e17823f76dfe3221a9
SS-X <zss2366@gmail.com>
2015-12-02 18:39:00 +0800

    3.12

--- chapter3/3.12.rkt
+++ chapter3/3.12.rkt
@@ -1,1 +1,30 @@
-#lang racket
+#lang r5rs
+
+(define (append x y)
+  (if (null? x)
+      y
+      (cons (car x) (append (cdr x) y))))
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+(define (append! x y)
+  (set-cdr! (last-pair x) y)
+  x)
+
+(define x (list 'a 'b))
+(define y (list 'c 'd))
+
+(define z (append x y))
+(display z)
+
+(display (cdr x))
+; (b)
+
+(define w (append! x y))
+
+(display w)
+
+(display (cdr x))
+;; (b c d)
