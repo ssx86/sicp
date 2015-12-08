370184b0a7909f04dd8b0b83e56d502d079fb95b
ssx86 <zss2366@sina.com>
2015-12-08 23:26:16 +0800

    3.14

--- chapter3/3.14.rkt
+++ chapter3/3.14.rkt
@@ -1,1 +1,14 @@
-#lang racket
+#lang r5rs
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+;; 这段代码的作用是链表倒置
+
+(define x (mystery '(a b b a a b b b )))
+(display x)
