acca900746b498c642be0bc5a51c958fcc603e13
SS-X <zss2366@gmail.com>
2015-12-09 18:19:39 +0800

    3.17

--- chapter3/3.17.rkt
+++ chapter3/3.17.rkt
@@ -1,1 +1,21 @@
 #lang racket
+
+(define count-pairs
+  (let ([point-list null])
+    (lambda (x)
+      (if (not (pair? x))
+          0
+          (if (member x point-list)
+              (+ (count-pairs (car x))
+                 (count-pairs (cdr x)))
+              (begin
+                (set! point-list (cons x point-list))
+                (+ (count-pairs (car x))
+                   (count-pairs (cdr x))
+                   1)))))))
+
+(define x (cons 'a 'a))
+(define y (cons x x))
+(define z (cons y y))
+
+(display (count-pairs z))
