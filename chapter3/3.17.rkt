db1c994dde9f63e397a243357af84ee4e24cd2e1
ssx86 <zss2366@gmail.com>
2015-12-10 23:46:21 +0800

    3.17 fix

--- chapter3/3.17.rkt
+++ chapter3/3.17.rkt
@@ -1,21 +1,22 @@
 #lang racket
 
-(define count-pairs
+(define (count-pairs x)
   (let ([point-list null])
-    (lambda (x)
-      (if (not (pair? x))
+    (define (test lst)
+      (if (or (not (pair? lst))
+              (memq lst point-list))
           0
-          (if (member x point-list)
-              (+ (count-pairs (car x))
-                 (count-pairs (cdr x)))
-              (begin
-                (set! point-list (cons x point-list))
-                (+ (count-pairs (car x))
-                   (count-pairs (cdr x))
-                   1)))))))
+          (begin
+            (set! point-list (cons lst point-list))
+            (+ (test (car lst))
+               (test (cdr lst))
+               1))))
+    (test  x)))
 
 (define x (cons 'a 'a))
 (define y (cons x x))
 (define z (cons y y))
 
 (display (count-pairs z))
+;; 第一次做的完全错了。不仅定义成了一个变量而不是函数，而且逻辑上，遇到之前存在的节点也没有停止，还继续探索左右子节点。
+;; 现在修正了。
