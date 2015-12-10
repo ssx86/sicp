66307f38ed2a9f97edeebcf4b2d723be761afbaa
ssx86 <zss2366@gmail.com>
2015-12-10 23:53:03 +0800

    3.18

--- chapter3/3.18.rkt
+++ chapter3/3.18.rkt
@@ -1,1 +1,22 @@
 #lang racket
+
+(define (circle x)
+  (let ([point-list null])
+    (define (helper lst)
+      (cond [(not (pair? lst)) #f]
+            [(memq lst point-list) #t]
+            [else
+             (begin
+               (set! point-list (cons lst point-list))
+               (or (helper (car lst))
+                   (helper (cdr lst))))]))
+    (helper x)))
+
+(define x (cons 'a 'a))
+(define y (cons 'b 'b))
+(define z (cons x y))
+
+(display (circle z))
+
+(define z2 (cons x x))
+(display (circle z2))
