c17fd82d51298b627a640c69fb007efe7226cb56
ssx86 <zss2366@sina.com>
2015-12-14 23:50:30 +0800

    3.22

--- chapter3/3.22.rkt
+++ chapter3/3.22.rkt
@@ -1,1 +1,79 @@
-#lang racket
+#lang r5rs
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+
+(define (make-queue) (cons '() '()))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (display "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (front-insert-queue! queue item)
+  (if (empty-queue? queue)
+      (let ((new-pair (cons item '())))
+        (begin
+          (set-front-ptr! queue new-pair)
+          (set-rear-ptr! queue new-pair))
+        queue)
+      (let ((new-pair (cons item (front-ptr queue))))
+        (set-front-ptr! queue new-pair)
+        queue)))
+
+
+(define (rear-insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (front-delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (display "DELETE! called with an empty queue" queue))
+        (else
+         (set-front-ptr! queue (cdr (front-ptr queue)))
+         queue)))
+
+(define (rear-delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (display "DELETE! called with an empty queue" queue))
+        (else
+         (set-cdr! queue (cons (car (rear-ptr queue)) '())))))
+
+(define (print-queue queue)
+  (display (front-ptr queue))
+  (newline))
+
+(define q1 (make-queue))
+(print-queue q1)
+
+(front-insert-queue! q1 'a)
+(print-queue q1)
+
+(front-insert-queue! q1 'b)
+(print-queue q1)
+
+(front-insert-queue! q1 'c)
+(print-queue q1)
+
+(rear-insert-queue! q1 'd)
+(print-queue q1)
+
+(rear-delete-queue! q1)
+(print-queue q1)
+
+(rear-delete-queue! q1)
+(print-queue q1)
+
+(front-delete-queue! q1)
+(print-queue q1)
