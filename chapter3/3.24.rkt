84ef92a47a958869a43aef4cd3d634974a14226d
SS-X <zss2366@gmail.com>
2015-12-15 18:50:07 +0800

    3.24 typed

--- chapter3/3.24.rkt
+++ chapter3/3.24.rkt
@@ -1,1 +1,22 @@
-#lang racket
+#lang r5rs
+
+(define (make-table)
+  (list '*table*))
+(define (lookup key table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (cdr record)
+        #f)))
+(define (insert! key value table)
+  (let ((record (assoc key (cdr table))))
+    (if record
+        (set-cdr! record value)
+        (set-cdr! table
+                  (cons (cons key value) (cdr table))))))
+
+(define t (make-table))
+(insert! 'a 3 t)
+(insert! 'b 4 t)
+(insert! 'b 5 t)
+(display (lookup 'a t))
+(display (lookup 'b t))
