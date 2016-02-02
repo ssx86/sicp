c8330d8b5283fc8e60608ec40992e490c3401f62
SS-X <zss2366@gmail.com>
2016-02-02 18:40:38 +0800

    3.28

--- chapter3/3.28.rkt
+++ chapter3/3.28.rkt
@@ -1,1 +1,72 @@
 #lang racket
+
+(define a (make-wire))
+(define b (make-wire))
+(define c (make-wire))
+
+(define d (make-wire))
+(define e (make-wire))
+(define s (make-wire))
+
+(define (make-wire) 1)
+
+(define (half-adder a b s c)
+  (let ([d (make-wire)] [e (make-wire)])
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+(define (full-adder a b c-in sum c-out)
+  (let ([s (make-wire)]
+        [c1 (make-wire)]
+        [c2 (make-wire)]
+        )
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    'ok))
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (λ ()
+                     (set-signal! output new-value)))))
+    (add-action! input invert-input)
+    'ok)
+
+(define (logical-not a)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (λ ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+(define (or-gate a1 a2 outout)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (λ ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+(define (logical-and a b)
+  (if (and (= a 0)
+           (= b 0))
+      0
+      1))
+(define (logical-or a b)
+  (if (or (= a 1)
+          (= b 1))
+      1
+      0))
